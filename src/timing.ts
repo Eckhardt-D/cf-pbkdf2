1b0f8dd86149bfff73e3aaf53c94034c6664394a
Eckhardt-D <eckhardt.dreyer@gmail.com>
2024-08-08 15:24:13 +0200

    feat: support timingSafeEqual for other envs

--- src/timing.ts
+++ src/timing.ts
@@ -2,7 +2,11 @@ import type { SubtleCrypto } from '@cloudflare/workers-types/experimental';
 import { subtle } from 'uncrypto';
 
 export async function timingSafeEqual(a: Uint8Array, b: Uint8Array): Promise<boolean> {
-  if (globalThis.navigator?.userAgent === 'Cloudflare-Workers') {
+  if (
+    globalThis.navigator?.userAgent === 'Cloudflare-Workers' ||
+    // @ts-expect-error: timingSafeEqual is not a standard API
+    globalThis.crypto?.subtle?.timingSafeEqual !== undefined
+  ) {
     return (subtle as unknown as SubtleCrypto).timingSafeEqual(a, b)
   }
 
